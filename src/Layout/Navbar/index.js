b494a06f32a32f83b03364f6e318875617d38d42
jaydipvaland <127921345+jaydipvaland@users.noreply.github.com>
2024-07-22 14:09:14 +0530

    navbar changes

--- src/Layout/Navbar/index.js
+++ src/Layout/Navbar/index.js
@@ -3,82 +3,138 @@ import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 
 const Navbar = ({ currentRoute }) => {
-	const navigate = useNavigate()
-	const location = useLocation()
+  const navigate = useNavigate();
+  const location = useLocation();
 
-	const Reports = ["/Dashboard", '/revenue',"/pockerrevenue",'/pointtransfer',"/mutiplayerpointtransfer","/dailystatus"]
-	const DrawDetails = ['/funtarget', "/funroullet", "/triplefun", "/funab"]
-	const agentmail = ['/agentmail']
-	const weekreport = ['/weekreport']
-	
+  const Reports = [
+    "/Dashboard",
+    "/revenue",
+    "/pockerrevenue",
+    "/pointtransfer",
+    "/mutiplayerpointtransfer",
+    "/dailystatus",
+  ];
+  const DrawDetails = ["/funtarget", "/funroullet", "/triplefun", "/funab"];
+  const agentmail = ["/agentmail"];
+  const weekreport = ["/weekreport"];
+
   return (
     <nav>
       <ul className="toresponsive" id="menu4">
         <li>
-          <a onClick={()=>navigate('/revenue')}  className={currentRoute === "Reports" || Reports?.includes(location?.pathname) ? "active main_a" : " main_a"}>
+          <a
+            className={
+              currentRoute === "Reports a_pointer" ||
+              Reports?.includes(location?.pathname)
+                ? "active main_a a_pointer"
+                : " main_a a_pointer"
+            }
+          >
             Reports
           </a>
           <ul>
             <li>
-              <a onClick={()=>navigate('/revenue')}>Revenue</a>
+              <a className="a_pointer" onClick={() => navigate("/revenue")}>
+                Revenue
+              </a>
             </li>
-            <li className="new-opt">
-              <a onClick={()=>navigate("/pockerrevenue")}>Poker Revenue</a>
-            </li>
-            <li>
-              <a onClick={()=>navigate('/pointtransfer')}>Point Transfer</a>
-            </li>
-            <li>
-              <a onClick={()=>navigate("/mutiplayerpointtransfer")}>Multiplayer Point Transfer</a>
+            <li className="a_pointer">
+              <a onClick={() => navigate("/pockerrevenue")}>Poker Revenue</a>
             </li>
             <li>
-              <a onClick={()=>navigate("/dailystatus")}>Daily Status</a>
+              <a
+                className="a_pointer"
+                onClick={() => navigate("/pointtransfer")}
+              >
+                Point Transfer
+              </a>
+            </li>
+            <li>
+              <a
+                className="a_pointer"
+                onClick={() => navigate("/mutiplayerpointtransfer")}
+              >
+                Multiplayer Point Transfer
+              </a>
+            </li>
+            <li>
+              <a className="a_pointer" onClick={() => navigate("/dailystatus")}>
+                Daily Status
+              </a>
             </li>
           </ul>
         </li>
         <li>
           <a
-           onClick={()=>navigate("/funtarget")}
-            className={currentRoute === "drawDetails" ||  DrawDetails?.includes(location?.pathname)  ? "active main_a" : " main_a"}
+            className={
+              currentRoute === "drawDetails a_pointer" ||
+              DrawDetails?.includes(location?.pathname)
+                ? "active main_a a_pointer"
+                : " main_a a_pointer"
+            }
           >
             Draw Details
           </a>
           <ul>
             <li>
-              <a onClick={()=>navigate("/funtarget")}>Fun Target</a>
+              <a className="a_pointer" onClick={() => navigate("/funtarget")}>
+                Fun Target
+              </a>
             </li>
             <li>
-              <a onClick={()=>navigate("/funroullet")}>Fun Roullet</a>
+              <a className="a_pointer" onClick={() => navigate("/funroullet")}>
+                Fun Roullet
+              </a>
             </li>
             <li>
-              <a onClick={()=>navigate("/triplefun")}>Triple Fun</a>
+              <a className="a_pointer" onClick={() => navigate("/triplefun")}>
+                Triple Fun
+              </a>
             </li>
             <li>
-              <a onClick={()=>navigate("/funab")}>Fun AB</a>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <a  onClick={()=>navigate("/agentmail")} className={currentRoute === "mailReport" || agentmail?.includes(location?.pathname) ? "active main_a" : "main_a"}>
-            Mail Report
-          </a>
-          <ul>
-            <li>
-              <a onClick={()=>navigate("/agentmail")}>Agent Mail</a>
+              <a className="a_pointer" onClick={() => navigate("/funab")}>
+                Fun AB
+              </a>
             </li>
           </ul>
         </li>
         <li>
           <a
-            onClick={()=>navigate("/weekreport")}
-            className={currentRoute === "weeklyDetailesReport" || weekreport?.includes(location?.pathname) ? "active main_a" : "main_a"}
+            className={
+              currentRoute === "mailReport a_pointer" ||
+              agentmail?.includes(location?.pathname)
+                ? "active main_a a_pointer"
+                : "main_a a_pointer"
+            }
+          >
+            Mail Report
+          </a>
+          <ul>
+            <li>
+              <a className="a_pointer" onClick={() => navigate("/agentmail")}>
+                Agent Mail
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a
+            onClick={() => navigate("/weekreport")}
+            className={
+              currentRoute === "weeklyDetailesReport a_pointer" ||
+              weekreport?.includes(location?.pathname)
+                ? "active main_a a_pointer"
+                : "main_a a_pointer"
+            }
           >
             Weekly Details Reports
           </a>
           <ul></ul>
         </li>
         <li>
-          <a className="main_a" onClick={()=>navigate("/")}>LogOut</a>
+          <a className="main_a a_pointer" onClick={() => navigate("/")}>
+            LogOut
+          </a>
           <ul></ul>
         </li>
       </ul>

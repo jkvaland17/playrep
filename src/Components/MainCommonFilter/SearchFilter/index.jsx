907c326c3973b8d6136605664b7f991e5b056df8
AnilBhil <130439462+AnilBhil@users.noreply.github.com>
2024-07-22 11:36:59 +0530

    style fix filter

--- src/Components/MainCommonFilter/SearchFilter/index.jsx
+++ src/Components/MainCommonFilter/SearchFilter/index.jsx
@@ -92,11 +92,30 @@ const SearchFilter = ({
             name="search"
             value={filterData?.search}
             onChange={handleChange}
+            endAdornment={filterData.filterClose && filterData.search !== "" && (
+              <p onClick={filterSearchHandler}>
+                <svg
+                  viewBox="0 0 24 24"
+                  x="1008"
+                  y="432"
+                  fit=""
+                  height="28"
+                  width="25"
+                  preserveAspectRatio="xMidYMid meet"
+                  focusable="false"
+                >
+                  <path
+                    d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"
+                    fill="#64748b"
+                  />
+                </svg>
+              </p>
+            )}
             onKeyDown={handleKeyPress} 
           />
         </Tooltip>
 
-        {filterData.filterClose && filterData.search !== "" && (
+        {/* {filterData.filterClose && filterData.search !== "" && (
           <p onClick={filterSearchHandler}>
             <svg
               viewBox="0 0 24 24"
@@ -114,9 +133,8 @@ const SearchFilter = ({
               />
             </svg>
           </p>
-        )}
+        )} */}
       </FormControl>
-
       <Button
         variant="contained"
         className={

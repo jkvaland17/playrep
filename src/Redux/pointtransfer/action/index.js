d0e6bf9c15f1ce783ae59ab3aa513b8ad5d98c39
jaydipvaland <127921345+jaydipvaland@users.noreply.github.com>
2024-07-24 16:22:42 +0530

    pointtransfer

--- src/Redux/pointtransfer/action/index.js
+++ src/Redux/pointtransfer/action/index.js
@@ -2,7 +2,7 @@ import { POINT_TRANSFER } from "../../route";
 
 export const getPointTransferList = (payload) => async (dispatch, getState, api) => {
     return await api
-        .post(POINT_TRANSFER, payload)
+        .get(POINT_TRANSFER, payload)
         .then((res) => {
             return res;
         })

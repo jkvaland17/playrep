e8ff35eb48eee907ab42a3c763df5a7063a66e2b
AnilBhil <130439462+AnilBhil@users.noreply.github.com>
2024-07-25 16:52:29 +0530

    fix loader btn

--- src/Components/FileButton/index.jsx
+++ src/Components/FileButton/index.jsx
@@ -4,7 +4,7 @@ import CircularProgress from '@mui/material/CircularProgress';
 const FilledButton = ({ type = "button", className = "btn btn_gray", value = "Clear", loading, disabledLoading, loader_class, ...rest }) => {
     return (
         <button disabled={loading} {...rest} type={type} className={className}>
-            {(true) ?
+            {(loading && !disabledLoading) ?
                 <div className={loader_class ? `btn-loader ${loader_class} btn-loader-small` : "btn-loader btn-loader-login"}>
                       <CircularProgress  size={20}/>
                 </div> : value}

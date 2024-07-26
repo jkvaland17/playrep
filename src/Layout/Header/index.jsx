e049024d12341f0cf58c72091c6d48f302864c25
jaydipvaland <127921345+jaydipvaland@users.noreply.github.com>
2024-07-26 09:48:03 +0530

    clean code

--- src/Layout/Header/index.jsx
+++ src/Layout/Header/index.jsx
@@ -1,57 +1,43 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import Navbar from "../Navbar";
 import logo from "../../assets/images/logo.png";
-import { userDetails } from '../../utils'
-import Typography from '@mui/material/Typography';
-import Popover from '@mui/material/Popover';
-import Cookies from 'js-cookie';
-import { isAuthenticated } from '../../utils';
+import { userDetails } from "../../utils";
+import { isAuthenticated } from "../../utils";
 
 const Index = () => {
-  const [userDate , setUserDate] = useState()
+  const [userDate, setUserDate] = useState();
   const [anchorEl, setAnchorEl] = React.useState(null);
 
-  useEffect(()=>{
-    setUserDate(userDetails())
-  },[isAuthenticated()])
+  useEffect(() => {
+    setUserDate(userDetails());
+  }, [isAuthenticated()]);
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
 
-
-    return (
-        <div>
-            <div className="header">
-            <div className="logo">
-              <img src={logo} alt="logo" />
-            </div>
-         {isAuthenticated() &&  <>
-          <div class="user-details" style={{cursor:"pointer"}}>
+  return (
+    <div>
+      <div className="header">
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
+        {isAuthenticated() && (
+          <>
+            <div class="user-details" style={{ cursor: "pointer" }}>
               <span>Welcome</span>
               <strong id="logout">
-              &nbsp;
-                <em  onClick={handleClick}>{userDate?.first_name}</em>
+                &nbsp;
+                <em onClick={handleClick}>{userDate?.first_name}</em>
                 &nbsp;
               </strong>
             </div>
             <Navbar />
-          </>}
-          </div>
-          {/* <Popover
-        id={id}
-        open={open}
-        anchorEl={anchorEl}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
-        }}
-      >
-        <Typography sx={{ p: 1, cursor:"pointer"}}>LogOut</Typography>
-      </Popover> */}
-        </div>
-    );
-}
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Index;

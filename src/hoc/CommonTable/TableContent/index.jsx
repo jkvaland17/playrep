2186d0d2bd507e59734598f2c6372cbf9b16fc78
AnilDevelo <mr.ab6354@gmail.com>
2024-07-27 11:13:46 +0530

    remove bg

--- src/hoc/CommonTable/TableContent/index.jsx
+++ src/hoc/CommonTable/TableContent/index.jsx
@@ -2,60 +2,64 @@ import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
 
 const TableContent = ({
-  rowData,
-  stableSort,
-  getComparator,
-  headCells,
-  order,
-  orderBy,
+	rowData,
+	stableSort,
+	getComparator,
+	headCells,
+	order,
+	orderBy,
 }) => {
-  return (
-    <>
-      {rowData?.length ? (
-        stableSort(rowData, getComparator(order, orderBy)).map(
-          (_data, dataIndex) => {
-            return (
-              <>
-                <TableRow
-                  className={"table_body"}
-                  hover
-                  role="checkbox"
-                  tabIndex={-1}
-                  key={_data.name}
-                >
-                  {headCells?.map((col, i) => {
-                    switch (col.type) {
-                      case "custom": {
-                        return col.render(_data, dataIndex);
-                      }
-                      case "action": {
-                        return col.ActionContent(_data);
-                      }
-                      case "hide": {
-                        return <TableCell className={"hide_table_td"} />;
-                       }
-                       default: {
-                        return <TableCell>{_data[col?.id]}</TableCell>;
-                       }
-                    }
-                  })}
-                </TableRow>
-              </>
-            );
-          }
-        )
-      ) : (
-        <TableRow hover role="checkbox" tabIndex={-1} className={"table_row"}>
-          <TableCell
-            colSpan={headCells?.length}
-            className={"data_notFound_box"}
-          >
-            No Data Found
-          </TableCell>{" "}
-        </TableRow>
-      )}
-    </>
-  );
+	return (
+		<>
+			{rowData?.length ? (
+				stableSort(rowData, getComparator(order, orderBy)).map(
+					(_data, dataIndex) => {
+
+						return (
+							<>
+								<TableRow
+									className={"table_body"}
+		
+									sx={{
+										backgroundColor: dataIndex % 2 === 0 ? "#e3f2fd" : "#fff",
+									}}
+									role="checkbox"
+									tabIndex={-1}
+									key={_data.name}
+								>
+									{headCells?.map((col, i) => {
+										switch (col.type) {
+											case "custom": {
+												return col.render(_data, dataIndex);
+											}
+											case "action": {
+												return col.ActionContent(_data);
+											}
+											case "hide": {
+												return <TableCell className={"hide_table_td"} />;
+											}
+											default: {
+												return <TableCell>{_data[col?.id]}</TableCell>;
+											}
+										}
+									})}
+								</TableRow>
+							</>
+						);
+					}
+				)
+			) : (
+				<TableRow hover role="checkbox" tabIndex={-1} className={"table_row"}>
+					<TableCell
+						colSpan={headCells?.length}
+						className={"data_notFound_box"}
+					>
+						No Data Found
+					</TableCell>{" "}
+				</TableRow>
+			)}
+		</>
+	);
 };
 
 export default TableContent;

53c32b46aa1aeb65f9212b3ee2ac97a371c45215
jaydipvaland <127921345+jaydipvaland@users.noreply.github.com>
2024-08-06 16:34:57 +0530

    dashboard changes

--- src/Pages/DashBoard/index.js
+++ src/Pages/DashBoard/index.js
@@ -1,27 +1,19 @@
-import React, { useState } from "react";
-import { data } from "../DashBoard/data";
+import React, { useEffect, useState } from "react";
 import CustomTable from "../../hoc/CommonTable";
 import MainCommonFilter from "../../Components/MainCommonFilter";
 import moment from "moment";
-import { Paper } from "@mui/material";
+import { Paper, TableCell } from "@mui/material";
+import { renderSrNo } from "../../utils";
+import { useDispatch } from "react-redux";
+import { getFunTargetList } from "../../Redux/drawDetails/action";
 
-let columns = [
-  {
-    id: "fullName",
-    label: "fullName",
-  },
-  {
-    id: "deviceId",
-    label: "deviceId",
-  },
-  {
-    id: "country",
-    label: "country",
-  },
-];
+
 
 const DashBoard = () => {
   const [pagination, setPagination] = useState({ rowsPerPage: 10, page: 0 });
+  const dispatch = useDispatch();
+  const [rowData, setRowData] = useState({ list: [], totalDocs: 0 });
+  const [loader, setLoader] = useState(false);
   const [filterData, setFilterData] = useState({
     startDate: null,
     endDate: null,
@@ -34,6 +26,54 @@ const DashBoard = () => {
     platformName: "All Users",
     state: "All States",
   });
+
+  let columns = [
+    {
+      id: "",
+      twoLineText: true,
+      label: "Sr. no.",
+      type: "custom",
+      render: (row, i) => renderSrNo(row, i, pagination),
+    },
+    {
+      id: "selectNumber",
+      label: "Winning No",
+    },
+    {
+      id: "updatedAt",
+      label: "Drow Time",
+      twoLineText: true,
+      type: "custom",
+      render: (row) => {
+        return (
+          <TableCell>
+            {moment(row?.updatedAt).format("MMM DD YYYY, hh:mm A")}
+          </TableCell>
+        );
+      },
+    },
+  ];
+
+  useEffect(() => {
+    getFunRoulletListData();
+  }, [pagination.rowsPerPage, pagination.page]);
+
+  const getFunRoulletListData = () => {
+    setLoader(true);
+    let payload = {
+      limit: pagination.rowsPerPage,
+      start: (pagination.page + 1 - 1) * pagination.rowsPerPage,
+      gameType: "FUN_TARGET",
+    };
+    dispatch(getFunTargetList(payload)).then((res) => {
+      setLoader(false);
+      setRowData({
+        ...rowData,
+        list: res?.data?.data?.docs,
+        totalDocs: res?.data?.data?.totalDocs,
+      });
+    });
+  };
 
   return (
     <Paper
@@ -61,10 +101,11 @@ const DashBoard = () => {
       </div>
       <CustomTable
         headCells={columns}
-        rowData={data.data.docs}
-        totalDocs={data?.data?.totalDocs}
+        rowData={rowData?.list}
+        totalDocs={rowData?.totalDocs}
         pagination={pagination}
         setPagination={setPagination}
+        loading={loader}
       />
     </Paper>
   );
